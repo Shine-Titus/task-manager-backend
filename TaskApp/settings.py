ae56c12bf44a71f0e779f8600e43d39be347fc7a
Shine Titus <r.shinetitus@gmail.com>
2025-08-19 12:27:25 +0530

    fixed cors

--- TaskApp/settings.py
+++ TaskApp/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://shine-titus.github.io/task-manager-frontend"
+    "https://shine-titus.github.io"
 ]
 
 
